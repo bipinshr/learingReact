d4fb352c77bfb53e07fe457447e91f2de43eda7e
Bipin Shrestha <bipinshrestha@Bipins-MBP.lan>
2023-08-17 15:34:35 -0500

    added useState

--- Lession3/newapp/src/App.js
+++ Lession3/newapp/src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [weather, setweather] = useState("Sunny");
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Current weather is {weather}</h1>
+      <button onClick={() => setweather("Raining")}> Raining</button>
+      <button onClick={() => setweather("Snowing")}> Snowing</button>
     </div>
   );
 }
