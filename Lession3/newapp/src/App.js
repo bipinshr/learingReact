cc7eb9a77f4c9131f5529910b094fe6bc01c4bed
Bipin Shrestha <bipinshrestha@Bipins-MBP.lan>
2023-08-17 16:10:53 -0500

    added useReducer

--- Lession3/newapp/src/App.js
+++ Lession3/newapp/src/App.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect, useReducer } from "react";
 import "./App.css";
 
 function App() {
   const [weather, setweather] = useState("Sunny");
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
+  useEffect(() => {
+    console.log(`wether right now is ${weather}`);
+  }, [weather]);
   return (
     <div className="App">
       <h1>Current weather is {weather}</h1>
       <button onClick={() => setweather("Raining")}> Raining</button>
       <button onClick={() => setweather("Snowing")}> Snowing</button>
+      <input type="checkbox" value={checked} onClick={setChecked} />
+      <label>{checked ? "checked" : "not Checked"}</label>
     </div>
   );
 }
