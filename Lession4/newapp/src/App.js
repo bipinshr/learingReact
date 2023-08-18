8bfd9993ffc9d8b058f57489576e426f63ad94ed
Bipin Shrestha <bipinshrestha@Bipins-MBP.lan>
2023-08-17 22:55:45 -0500

    added renderd props with Nepal Mountain

--- Lession4/newapp/src/App.js
+++ Lession4/newapp/src/App.js
@@ -10,7 +10,30 @@ function Github({ name, location, avatar }) {
     </div>
   );
 }
+const nepal_mountains = [
+  { name: "Mount Everest", altitude: 8848 },
+  { name: "Kangchenjunga", altitude: 8586 },
+  { name: "Lhotse", altitude: 8516 },
+  { name: "Makalu", altitude: 8485 },
+  { name: "Cho Oyu", altitude: 8188 },
+  { name: "Dhaulagiri", altitude: 8167 },
+  { name: "Manaslu", altitude: 8163 },
+  { name: "Annapurna", altitude: 8091 },
+  { name: "Gyachung Kang", altitude: 7952 },
+  { name: "Pumori", altitude: 7161 },
+];
 
+function List({ data, renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
+}
 function App() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
@@ -31,7 +54,21 @@ function App() {
   if (!data) return null;
   //else
   // return <pre>{JSON.stringify(data, null, 2)}</pre>; //nu;;.2 for formating
-  return <Github name={data.login} location={data.created_at} avatar={data.avatar_url} />;
+  return (
+    <div>
+      <Github name={data.login} location={data.created_at} avatar={data.avatar_url} />
+      <h1>Mountains in Nepal</h1>
+      <List
+        data={nepal_mountains}
+        renderEmpty={<p>Cannot get deta for Nepal Montain right now</p>}
+        renderItem={(item) => (
+          <>
+            {item.name} - {item.altitude} ft.
+          </>
+        )}
+      />
+    </div>
+  );
 }
 
 export default App;
