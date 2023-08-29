80dc0b87b423d97c83d99151cf4bb3e40c37f57a
Bipin Shrestha <bipinshrestha@Bipins-MBP.lan>
2023-08-29 13:39:23 -0500

    Addede Nested link

--- Lession5/react-app/src/App.js
+++ Lession5/react-app/src/App.js
@@ -1,17 +1,34 @@
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 
 function Home() {
   return (
     <div>
+      <nav>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <h1>My Website</h1>
     </div>
   );
 }
-
+export function History() {
+  return (
+    <div>
+      <h1>This is our history</h1>
+    </div>
+  );
+}
 export function About() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <h1>About Us</h1>
+      <Outlet />
     </div>
   );
 }
@@ -19,6 +36,11 @@ export function About() {
 export function Contact() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <h1>Contact us anytime</h1>
     </div>
   );
